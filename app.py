e68de36643b7b6d926d9cbc0dcfa4f3a8d2f52fd
Edvards Mazprecnieks <edvards.mazprecnieks@code.berlin>
2023-03-13 17:33:48 +0100

    Create subjects page

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app.config.from_object('config')
 def schedule():
     return render_template('schedule.html')
 
+@app.route('/subject')
+def subjects():
+    return render_template('subjects_page.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=3000, debug=True)
