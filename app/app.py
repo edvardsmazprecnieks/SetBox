8ef4e44827296db66b8bbe6eba111ab185e61013
Philipp Gallaschik <p,gallaschik@gmail.com>
2023-03-24 13:24:04 +0100

    saved my try for the scheduling function and some further schedule

--- app/app.py
+++ app/app.py
@@ -4,6 +4,43 @@ import psycopg2
 from flask import Flask
 from . import subjects, lesson, schedule
 
+# def schedule():
+#     Maths = [
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 1, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0], 
+#     [0, 0, 0, 0, 0]
+#     ]
+   
+#     for list in Maths:
+#         hour = 8
+#         color = f"color{hour}"
+#         for n in list:
+#             daycounter = 0
+#             if n >= 0:
+#                 if daycounter == 0:
+#                     color = f"color{daycounter}m"
+#                 elif daycounter == 1:
+#                     color = f"color{daycounter}tu"
+#                 elif daycounter == 2:
+#                     color = f"color{daycounter}w"
+#                 elif daycounter == 3:
+#                     color = f"color{daycounter}th"
+#                 elif daycounter == 4:
+#                     color = f"color{daycounter}f"
+#                 else: 
+#                     color = ""
+#             daycounter += 1
+#         hour += 1
+#         colorwithstyle = "style='background: blue'"
+
+
 
 def get_database_connection():
     connection = psycopg2.connect(
