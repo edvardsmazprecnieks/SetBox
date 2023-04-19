11ebcda96cbb3632626c2f506e4a78e9a29e9219
Edvards Mazprecnieks <edvards.mazprecnieks@code.berlin>
2023-04-19 16:56:11 +0200

    Fix schedule

--- app/schedule/routes.py
+++ app/schedule/routes.py
@@ -1,17 +1,23 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request, url_for
 from flask_login import login_required
 from datetime import datetime, timedelta, time
 from app.extensions.database.models import Lesson, Subject
 from flask_login import current_user
+from app.extensions.database.crud import db
 
 blueprint = Blueprint('schedule', __name__)
 
 
-@blueprint.route('/schedule')
+@blueprint.get('/schedule')
 @login_required
 def schedule_no_date():
     return redirect('/schedule/' + datetime.now().strftime('%Y-%m-%d'))
 
+@blueprint.post('/schedule')
+@login_required
+def schedule_post_date():
+    date = request.form.get('date')
+    return redirect(url_for('schedule.schedule', date_string=date))
 
 @blueprint.route('/schedule/<date_string>')
 @login_required
@@ -40,13 +46,14 @@ def schedule(date_string):
     sunday_1 = sunday.strftime('%d.%m.%Y')
     weekdates = [monday_1, tuesday_1, wednesday_1, thursday_1, friday_1, saturday_1, sunday_1]
     for date in weekdates:
-        lessons = Lesson.query.filter(Lesson.formatted_date == date).filter(Lesson.subject_id == Subject.id).filter(Subject.owner_user_id == current_user.id).all()
+        #lessons = Lesson.query.filter(Lesson.formatted_date == date).filter(Lesson.subject_id == Subject.id).filter(Subject.owner_user_id == current_user.id).all()
+        lessons = db.session.query(Lesson, Subject).filter(Lesson.formatted_date == date).filter(Lesson.subject_id == Subject.id).filter(Subject.owner_user_id == current_user.id).all()
         lessons_list = lessons_list + lessons
         for lesson in lessons:
-            if lesson.start_time < min_time:
-                min_time = lesson.start_time
-            if lesson.end_time > max_time:
-                max_time = lesson.end_time
+            if lesson.Lesson.start_time < min_time:
+                min_time = lesson.Lesson.start_time
+            if lesson.Lesson.end_time > max_time:
+                max_time = lesson.Lesson.end_time
     min_time_rounded = min_time.replace(microsecond=0, second=0, minute=0)
     if max_time.replace(hour = 0) > time(minute = 0):
         max_time_rounded = max_time.replace(microsecond=0, second=0, minute=0, hour = max_time.hour + 1)
