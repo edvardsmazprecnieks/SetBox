90a0b38b91c7b1501da2538d2a359c78a801f032
Edvards Mazprecnieks <edvards.mazprecnieks@code.berlin>
2023-04-23 22:20:07 +0200

    Rename variables (clean up code)

--- app/schedule/routes.py
+++ app/schedule/routes.py
@@ -24,40 +24,39 @@ def schedule_post_date():
 @blueprint.route("/schedule/<date_string>")
 @login_required
 def schedule(date_string):
-    times = []
-    min_time = time(hour=23, minute=59, second=59)
-    max_time = time(hour=0, minute=0, second=0)
-    lessons_list = []
-    date = datetime.strptime(date_string, "%Y-%m-%d")
-    day_of_week = date.strftime("%w")
+    list_of_schedule_times = []
+    min_time_of_schedule = time(hour=23, minute=59, second=59)
+    max_time_of_schedule = time(hour=0, minute=0, second=0)
+    all_lessons_list = []
+    given_date = datetime.strptime(date_string, "%Y-%m-%d")
+    day_of_week = given_date.strftime("%w")
     if int(day_of_week) == 0:
         day_of_week = 7
-    monday = date + timedelta(days=-int(day_of_week) + 1)
-    tuesday = date + timedelta(days=-int(day_of_week) + 2)
-    wednesday = date + timedelta(days=-int(day_of_week) + 3)
-    thursday = date + timedelta(days=-int(day_of_week) + 4)
-    friday = date + timedelta(days=-int(day_of_week) + 5)
-    saturday = date + timedelta(days=-int(day_of_week) + 6)
-    sunday = date + timedelta(days=-int(day_of_week) + 7)
-    monday_1 = monday.strftime("%d.%m.%Y")
-    tuesday_1 = tuesday.strftime("%d.%m.%Y")
-    wednesday_1 = wednesday.strftime("%d.%m.%Y")
-    thursday_1 = thursday.strftime("%d.%m.%Y")
-    friday_1 = friday.strftime("%d.%m.%Y")
-    saturday_1 = saturday.strftime("%d.%m.%Y")
-    sunday_1 = sunday.strftime("%d.%m.%Y")
-    weekdates = [
-        monday_1,
-        tuesday_1,
-        wednesday_1,
-        thursday_1,
-        friday_1,
-        saturday_1,
-        sunday_1,
+    monday = given_date + timedelta(days=-int(day_of_week) + 1)
+    tuesday = given_date + timedelta(days=-int(day_of_week) + 2)
+    wednesday = given_date + timedelta(days=-int(day_of_week) + 3)
+    thursday = given_date + timedelta(days=-int(day_of_week) + 4)
+    friday = given_date + timedelta(days=-int(day_of_week) + 5)
+    saturday = given_date + timedelta(days=-int(day_of_week) + 6)
+    sunday = given_date + timedelta(days=-int(day_of_week) + 7)
+    monday_string = monday.strftime("%d.%m.%Y")
+    tuesday_string = tuesday.strftime("%d.%m.%Y")
+    wednesday_string = wednesday.strftime("%d.%m.%Y")
+    thursday_string = thursday.strftime("%d.%m.%Y")
+    friday_string = friday.strftime("%d.%m.%Y")
+    saturday_string = saturday.strftime("%d.%m.%Y")
+    sunday_string = sunday.strftime("%d.%m.%Y")
+    all_week_dates = [
+        monday_string,
+        tuesday_string,
+        wednesday_string,
+        thursday_string,
+        friday_string,
+        saturday_string,
+        sunday_string,
     ]
-    for date in weekdates:
-        # lessons = Lesson.query.filter(Lesson.formatted_date == date).filter(Lesson.subject_id == Subject.id).filter(Subject.owner_user_id == current_user.id).all()
-        lessons = (
+    for date in all_week_dates:
+        lessons_in_date = (
             db.session.query(Lesson, Subject)
             .filter(Lesson.formatted_date == date)
             .filter(Lesson.subject_id == Subject.id)
@@ -68,27 +67,32 @@ def schedule(date_string):
             )
             .all()
         )
-        lessons_list = lessons_list + lessons
-        for lesson in lessons:
-            if lesson.Lesson.start_time < min_time:
-                min_time = lesson.Lesson.start_time
-            if lesson.Lesson.end_time > max_time:
-                max_time = lesson.Lesson.end_time
-    min_time_rounded = min_time.replace(microsecond=0, second=0, minute=0)
-    if max_time.replace(hour=0) > time(minute=0):
-        max_time_rounded = max_time.replace(
-            microsecond=0, second=0, minute=0, hour=max_time.hour + 1
+        all_lessons_list = all_lessons_list + lessons_in_date
+        for lesson in lessons_in_date:
+            if lesson.Lesson.start_time < min_time_of_schedule:
+                min_time_of_schedule = lesson.Lesson.start_time
+            if lesson.Lesson.end_time > max_time_of_schedule:
+                max_time_of_schedule = lesson.Lesson.end_time
+    min_time_of_schedule_rounded = min_time_of_schedule.replace(
+        microsecond=0, second=0, minute=0
+    )
+    if max_time_of_schedule.replace(hour=0) > time(minute=0):
+        max_time_of_schedule_rounded = max_time_of_schedule.replace(
+            microsecond=0, second=0, minute=0, hour=max_time_of_schedule.hour + 1
         )
     else:
-        max_time_rounded = max_time.replace(microsecond=0, second=0)
-    if min_time_rounded > max_time_rounded:
+        max_time_rounded = max_time_of_schedule.replace(microsecond=0, second=0)
+    if min_time_of_schedule_rounded > max_time_rounded:
         return render_template(
-            "schedule/schedule.html", weekdates=weekdates, times=[], lessons=[]
+            "schedule/schedule.html", weekdates=all_week_dates, times=[], lessons=[]
         )
-    time_1 = min_time_rounded
-    while time_1 <= max_time_rounded:
-        times.append(time_1)
-        time_1 = time_1.replace(hour=time_1.hour + 1)
+    time_of_schedule = min_time_of_schedule_rounded
+    while time_of_schedule <= max_time_rounded:
+        list_of_schedule_times.append(time_of_schedule)
+        time_of_schedule = time_of_schedule.replace(hour=time_of_schedule.hour + 1)
     return render_template(
-        "schedule/schedule.html", weekdates=weekdates, times=times, lessons=lessons_list
+        "schedule/schedule.html",
+        weekdates=all_week_dates,
+        times=list_of_schedule_times,
+        lessons=all_lessons_list,
     )
