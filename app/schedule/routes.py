5b317fd16e12c055c5aa9bc6f3719a4813c8af9a
Edvards Mazprecnieks <edvards.mazprecnieks@code.berlin>
2023-04-24 12:50:03 +0200

    Extract method, extract & rename variables, fixes

--- app/schedule/routes.py
+++ app/schedule/routes.py
@@ -25,40 +25,44 @@ def schedule_post_date():
 @blueprint.route("/schedule/<date_string>")
 @login_required
 def schedule(date_string):
+    #lists that will be used for storing data
     list_of_schedule_times = []
+    all_lessons_list = []
+
     min_time_of_schedule = time(hour=23, minute=59, second=59)
     max_time_of_schedule = time(hour=0, minute=0, second=0)
-    all_lessons_list = []
+    
     given_date = datetime.strptime(date_string, "%Y-%m-%d")
-    given_day_of_week = given_date.strftime("%w")
-    if int(given_day_of_week) == 0:
-        given_day_of_week = 7
-    all_week_dates = [ ]
-    for day_of_week in range(1, 8):
-        day = given_date + timedelta(days=-int(given_day_of_week) + day_of_week)
-        day_string = day.strftime("%d.%m.%Y")
-        all_week_dates.append(day_string)
+
+    week_days_list = make_a_list_of_week_days(given_date)
+    first_day_of_week = datetime.strptime(week_days_list[0], "%d.%m.%Y")
+    last_day_of_week = datetime.strptime(week_days_list[6], "%d.%m.%Y")
+
     all_lessons_list = (db.session.query(Lesson, Subject)
         .filter(and_
-                (Lesson.date >= datetime.strptime(all_week_dates[0], "%d.%m.%Y")),
-                (Lesson.date <= datetime.strptime(all_week_dates[6], "%d.%m.%Y"))
+                (Lesson.date >= first_day_of_week),
+                (Lesson.date <= last_day_of_week)
         )
-        .filter(Lesson.subject_id == Subject.id)
-        .join(UserInSubject, Subject.id == UserInSubject.subject_id, full = True)
-        .filter(or_
-            (Subject.owner_user_id == current_user.id),
+        .join(Subject)
+        .join(UserInSubject, isouter=True)
+        .filter(
+            (Subject.owner_user_id == current_user.id)|
             (UserInSubject.user_id == current_user.id)
         )
         .all()
     )
+
+    # maybe replace with db query?
     for lesson in all_lessons_list:
         if lesson.Lesson.start_time < min_time_of_schedule:
             min_time_of_schedule = lesson.Lesson.start_time
         if lesson.Lesson.end_time > max_time_of_schedule:
             max_time_of_schedule = lesson.Lesson.end_time
+
     min_time_of_schedule_rounded = min_time_of_schedule.replace(
         microsecond=0, second=0, minute=0
     )
+
     if max_time_of_schedule.replace(hour=0) > time(minute=0):
         max_time_of_schedule_rounded = max_time_of_schedule.replace(
             microsecond=0, second=0, minute=0, hour=max_time_of_schedule.hour + 1
@@ -67,15 +71,28 @@ def schedule(date_string):
         max_time_of_schedule_rounded = max_time_of_schedule.replace(microsecond=0, second=0)
     if min_time_of_schedule_rounded > max_time_of_schedule_rounded:
         return render_template(
-            "schedule/schedule.html", weekdates=all_week_dates, times=[], lessons=[]
+            "schedule/schedule.html", weekdates=week_days_list, times=[], lessons=[]
         )
+    
     time_of_schedule = min_time_of_schedule_rounded
     while time_of_schedule <= max_time_of_schedule_rounded:
         list_of_schedule_times.append(time_of_schedule)
         time_of_schedule = time_of_schedule.replace(hour=time_of_schedule.hour + 1)
+
     return render_template(
         "schedule/schedule.html",
-        weekdates=all_week_dates,
+        weekdates=week_days_list,
         times=list_of_schedule_times,
         lessons=all_lessons_list,
     )
+
+def make_a_list_of_week_days(date):
+    given_day_of_week = date.strftime("%w")
+    if int(given_day_of_week) == 0:
+        given_day_of_week = 7
+    week_dates = [ ]
+    for day_of_week in range(1, 8):
+        day = date + timedelta(days=-int(given_day_of_week) + day_of_week)
+        day_string = day.strftime("%d.%m.%Y")
+        week_dates.append(day_string)
+    return week_dates
